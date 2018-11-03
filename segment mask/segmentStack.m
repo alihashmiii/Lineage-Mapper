34eb89914a88c3f4372009359a87dde0cb428617
Ali Hashmi <alihashmi_87@hotmail.com>
2018-11-03 01:18:06 +0100

    Update segmentStack.m

--- segment mask/segmentStack.m
+++ segment mask/segmentStack.m
@@ -23,12 +23,29 @@ BeginPackage["segmentStack`"]
 (*Segment Image*)
 
 
-segmentImage[binarizedMask_?ImageQ,threshCellsize_:20000]:= Module[{seg,areas,indexMaxarea,maxArea},
+segmentImage[binarizedMask_?ImageQ,opt:"ConnectedComponents"|"Watershed":"Watershed",threshCellsize_:20000]:= Module[{seg,areas,
+indexMaxarea,maxArea,indsmallareas={},ind},
+ seg = Switch[opt,"ConnectedComponents",
+ MorphologicalComponents@*ColorNegate@Dilation[binarizedMask,1],
+ "Watershed",
+ WatershedComponents[binarizedMask,CornerNeighbors->False]
+ ];
+ areas = ComponentMeasurements[seg,"Area"];
+ {indexMaxarea,maxArea} = First@MaximalBy[areas,Last]/.Rule-> List;
+ indsmallareas = Keys@Cases[areas,HoldPattern[_-> 1.]];
+ If[maxArea >= threshCellsize || indsmallareas != {},
+ ind = {indexMaxarea}~Join~indsmallareas;
+ seg = ArrayComponents[seg,Length@areas,Thread[ind->0]]
+ ];
+ If[opt == "ConnectedComponents", seg~Dilation~1, seg]
+];
+
+(*segmentImage[binarizedMask_?ImageQ,threshCellsize_:20000]:= Module[{seg,areas,indexMaxarea,maxArea},
   seg=MorphologicalComponents@*ColorNegate@Dilation[binarizedMask,1];
   areas=ComponentMeasurements[seg,"Area"];
   {indexMaxarea,maxArea}=First@MaximalBy[areas,Last]/.Rule-> List;
   If[maxArea >= threshCellsize,ArrayComponents[seg,Length@areas,indexMaxarea->0],seg]~Dilation~1
-];
+];*)
 
 
 (* segmentImage[binarizedMask_?ImageQ, thresh_:1000]:= Module[{seg, mincount},
