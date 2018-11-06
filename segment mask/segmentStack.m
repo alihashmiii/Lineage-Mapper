d6c9025c3899a6a89875bcac5a37a1c4883dbcdd
Ali Hashmi <alihashmi_87@hotmail.com>
2018-11-06 20:49:23 +0100

    Update segmentStack.m

--- segment mask/segmentStack.m
+++ segment mask/segmentStack.m
@@ -18,41 +18,29 @@ BeginPackage["segmentStack`"]
 (* Exported symbols added here with SymbolName::usage *)
 
 
-
 (* ::Subsection:: *)
 (*Segment Image*)
 
 
 segmentImage[binarizedMask_?ImageQ,opt:"ConnectedComponents"|"Watershed":"Watershed",threshCellsize_:20000]:= Module[{seg,areas,
 indexMaxarea,maxArea,indsmallareas={},$ind},
- seg = Switch[opt,"ConnectedComponents",
- MorphologicalComponents@*ColorNegate@Dilation[binarizedMask,1],
- "Watershed",
- WatershedComponents[binarizedMask,CornerNeighbors->False]
- ];
- areas = ComponentMeasurements[seg,"Area"];
- {indexMaxarea,maxArea} = First@MaximalBy[areas,Last]/.Rule-> List;
- indsmallareas = Keys@Cases[areas,HoldPattern[_-> 1.]];
- If[maxArea >= threshCellsize || indsmallareas != {},
- $ind = {indexMaxarea}~Join~indsmallareas;
- seg = ArrayComponents[seg,Length@areas,Thread[$ind->0]]
- ];
- If[opt == "ConnectedComponents", seg~Dilation~1, seg]
+seg= Switch[opt,"ConnectedComponents",
+(* assuming we input 0 as foreground and 1 as background. ConnectedComponents is a more general segmentation
+framework *)
+MorphologicalComponents[ColorNegate@binarizedMask, CornerNeighbors->False],
+"Watershed",
+(* for epithelial cells *)
+WatershedComponents[binarizedMask, CornerNeighbors->False]
 ];
-
-(*segmentImage[binarizedMask_?ImageQ,threshCellsize_:20000]:= Module[{seg,areas,indexMaxarea,maxArea},
-  seg=MorphologicalComponents@*ColorNegate@Dilation[binarizedMask,1];
-  areas=ComponentMeasurements[seg,"Area"];
-  {indexMaxarea,maxArea}=First@MaximalBy[areas,Last]/.Rule-> List;
-  If[maxArea >= threshCellsize,ArrayComponents[seg,Length@areas,indexMaxarea->0],seg]~Dilation~1
-];*)
-
-
-(* segmentImage[binarizedMask_?ImageQ, thresh_:1000]:= Module[{seg, mincount},
-  seg = DeleteBorderComponents@MorphologicalComponents[ColorNegate@binarizedMask, CornerNeighbors->False];
-  mincount = Min@Values@ComponentMeasurements[seg,"Count"];
-  ArrayComponents@If[mincount <= thresh, DeleteSmallComponents[seg, thresh], seg]
-];*)
+areas=ComponentMeasurements[seg,"Area"];
+{indexMaxarea,maxArea}=First@MaximalBy[areas,Last]/.Rule-> List;
+indsmallareas = Keys@Cases[areas,HoldPattern[_-> 1.]];
+If[maxArea >= threshCellsize||indsmallareas!= {},
+$ind={indexMaxarea}~Join~indsmallareas;
+seg=ArrayComponents[seg,Length@areas,Thread[$ind->0]]
+];
+seg
+];
 
 
 Begin["`Private`"]
